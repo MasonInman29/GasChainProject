c683653bed9afdc8c9288a59d4b9a497225d53e8
Wessel-Lindsey <lindsey.k.wessel@gmail.com>
2024-10-31 17:16:30 -0500

    Create gasChain.java

---
+++ gasChain.java
@@ -0,0 +1,29 @@
+
+
+
+
+public class gasChain{
+    public static void main(String[] args){
+        
+
+
+
+    }
+}
+
+
+public class gasStation{
+
+
+    public gasStation(){
+
+    }
+}
+
+public class gasStore{
+    
+
+    public gasStore(){
+
+    }
+}
